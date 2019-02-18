3301a6b78d5681d5692f58948a72ed4f7e8a6c20
zlatan <zlatan.pham@gmail.com>
2019-02-18 13:19:05 +0700

    getStyle for render DRY code

--- src/SkeletonString.tsx
+++ src/SkeletonString.tsx
@@ -1,45 +1,45 @@
 import * as React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled, { CSSObject } from 'styled-components';
 import {
   SkeletonThemeConsumer,
   SkeletonThemeProps,
 } from './SkeletonThemeProvider';
+import { defaultBaseColor, defaultHighlightColor, getStyles } from './shared';
 
-export const defaultBaseColor = '#eee';
-export const defaultHighlightColor = '#f5f5f5';
-
-export const flash = keyframes`
-  0% {
-    background-position: -200px 0;
-  }
-  100% {
-    background-position: calc(200px + 100%) 0;
-  }
+const TextWrapper = styled.span<
+  SkeletonStringDefaultProps & { theme: SkeletonThemeProps }
+>`
+  ${props => getStyles(props)}
 `;
 
-const TextWrapper = styled.span<SkeletonThemeProps>`
-  background-color: ${props => props.baseColor || defaultBaseColor};
-  background-image: linear-gradient(
-    90deg,
-    ${props => props.baseColor || defaultBaseColor},
-    ${props => props.hightlightColor || defaultHighlightColor},
-    ${props => props.baseColor || defaultBaseColor}
-  );
-  background-size: 200px 100%;
-  background-repeat: no-repeat;
-  display: inline-block;
-  animation: ${flash} 1.5s ease-in-out infinite;
-  color: transparent !important;
-  > * {
-    color: transparent !important;
-  }
-`;
+interface SkeletonStringDefaultProps {
+  baseColor: string;
+  highlightColor: string;
+  customeStyle?: CSSObject | TemplateStringsArray;
+}
+export interface SkeletonStringProps {
+  baseColor?: string;
+  highlightColor?: string;
+  customeStyle?: CSSObject | TemplateStringsArray;
+}
 
-export class SkeletonString extends React.Component {
+export class SkeletonString extends React.Component<SkeletonStringProps> {
+  static defaultProps: SkeletonStringDefaultProps = {
+    baseColor: defaultBaseColor,
+    highlightColor: defaultHighlightColor,
+  };
+
   render() {
     return (
       <SkeletonThemeConsumer>
-        {props => <TextWrapper {...props}>{this.props.children}</TextWrapper>}
+        {theme => (
+          <TextWrapper
+            {...this.props as SkeletonStringDefaultProps}
+            theme={theme}
+          >
+            {this.props.children}
+          </TextWrapper>
+        )}
       </SkeletonThemeConsumer>
     );
   }
--- src/shared.ts
+++ src/shared.ts
@@ -1,2 +1,41 @@
+import { keyframes, CSSObject, css } from 'styled-components';
+import { SkeletonThemeProps } from './SkeletonThemeProvider';
 export const defaultBaseColor = '#eeeeee';
 export const defaultHighlightColor = '#f5f5f5';
+
+export const flash = keyframes`
+  0% {
+    background-position: -200px 0;
+  }
+  100% {
+    background-position: calc(200px + 100%) 0;
+  }
+`;
+
+interface DefaultProps {
+  baseColor: string;
+  highlightColor: string;
+  customeStyle?: CSSObject | TemplateStringsArray;
+}
+
+export const getStyles = (
+  props: DefaultProps & { theme: SkeletonThemeProps },
+) => {
+  return css`
+    background-color: ${props.theme.baseColor || props.baseColor};
+    background-image: linear-gradient(
+      90deg,
+      ${props.theme.baseColor || props.baseColor},
+      ${props.theme.hightlightColor || props.highlightColor},
+      ${props.theme.baseColor || props.baseColor}
+    );
+    background-size: 200px 100%;
+    background-repeat: no-repeat;
+    display: inline-block;
+    animation: ${flash} 1.5s ease-in-out infinite;
+    color: transparent !important;
+    > * {
+      color: transparent !important;
+    }
+  `;
+};
