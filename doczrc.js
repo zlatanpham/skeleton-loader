11a1bdef2655ddce9b12d5ba4e7e8dd6b4652526
zlatan <zlatan.pham@gmail.com>
2019-02-18 17:48:51 +0700

    integrate docz: init

---
+++ doczrc.js
@@ -0,0 +1,18 @@
+// doczrc.js
+export default {
+  title: 'Skeleton Screen',
+  description: 'Description',
+  source: './src',
+  typescript: true,
+  themeConfig: {
+    colors: {
+      primary: 'tomato',
+    },
+    source: './src',
+    styles: {
+      h1: {
+        fontSize: 100,
+      },
+    },
+  },
+};
