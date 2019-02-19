0939527280c7cc1dcd78f48542dbac4c9fa26329
zlatan <zlatan.pham@gmail.com>
2019-02-19 16:51:59 +0700

    add docz-netlify-plugin

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,8 @@
+import doczPluginNetlify from "docz-plugin-netlify";
+
 // doczrc.js
 export default {
+  plugins: [doczPluginNetlify()],
   title: 'Skeleton Screen',
   description: 'Description',
   source: './src',
